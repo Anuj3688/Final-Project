9d4e4d809278f199e923b671c9c0d250467b59c2
Anuj3688 <anuj.10tiwari2001@gmail.com>
2022-02-04 01:02:49 +0530

    JavaScript Basics added

---
+++ script.js
@@ -0,0 +1,15 @@
+function aboutvis(){
+    document.getElementById("abfunc").style = "display:flex";
+    
+}
+
+function aboutvisd() {
+    document.getElementById("abfunc").style = "display:none;";
+}
+
+function contactvis() {
+    document.getElementById("cofunc").style = "display:block";
+}
+function contactvisd() {
+    document.getElementById("cofunc").style = "display:none";
+}
