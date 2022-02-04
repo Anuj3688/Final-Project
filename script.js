31f5c4290111758eff0115585d825aebd22189ba
Anuj3688 <anuj.10tiwari2001@gmail.com>
2022-02-05 01:57:29 +0530

    Added some animation and score logic in sql page

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-function aboutvis(){
-    document.getElementById("abfunc").style = "display:flex";
-    
+// functions for contact and about us hover and inclick
+function aboutvis() {
+  document.getElementById("abfunc").style = "display:flex";
 }
 
 function aboutvisd() {
-    document.getElementById("abfunc").style = "display:none;";
+  document.getElementById("abfunc").style = "display:none;";
 }
 
 function contactvis() {
-    document.getElementById("cofunc").style = "display:block";
+  document.getElementById("cofunc").style = "display:block";
 }
 function contactvisd() {
-    document.getElementById("cofunc").style = "display:none";
+  document.getElementById("cofunc").style = "display:none";
 }
 
-
 function oabout() {
-    window.open("C:\Users\anuj1\OneDrive\Desktop\WEWA2\Projects\Final Project\aboutus.html");
+  window.open(
+    "C:Usersanuj1OneDriveDesktopWEWA2ProjectsFinal Projectaboutus.html"
+  );
 }
 
 function ocontact() {
-    window.open("C:\Users\anuj1\OneDrive\Desktop\WEWA2\Projects\Final Project\contact.html");
+  window.open(
+    "C:Usersanuj1OneDriveDesktopWEWA2ProjectsFinal Projectcontact.html"
+  );
 }
 
-function openfull(x) {
-    var idans  = "SQL"+x;
-    var h = document.getElementById(idans).style.height;
-    document.getElementById(idans).style = "height:unset";
+
+var sqls = 0;
+
+
+// to open the full tab of SQL
+function openfull(x , score) {
+  var idans = "SQL" + x;
+  var h = document.getElementById(idans).style.height;    
+  if (document.getElementById(idans).style.height > "86px") {
+    document.getElementById(idans).style.height = "86px";
+
+  } else {
+
+    if(document.getElementById(idans).querySelector(".visited").innerText == 0){
+        sqls += score;
+        document.getElementById(idans).querySelector(".visited").innerText = 1;
+        document.getElementById(idans).querySelector(".subheadsco").innerHTML = "Score Boost " + score+"/"+score;
+        document.getElementById("sqlscore").innerText = sqls+"/200"
+    }
+
+    document.getElementById(idans).style.height = "unset";
+  }
 }
+// if(document.getElementById(idans).getElementsByClassName("visited").innerText == 0){
+//     sqls += score;
+//     document.getElementById(idans).getElementsByClassName("visited").innerText = 1;
+//     document.getElementById("sqlscore").innerText = sqls+"/200"
+// }
